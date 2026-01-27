1ca085a378f991404ad49223846ae1757d8472d3
Bharat <bk@alpyne.tech>
2026-01-27 22:57:28 +0530

    API Gateway implemented with proxy routing to auth service

--- gateway/src/routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createProxyMiddleware } from "http-proxy-middleware";
-const router = Router();
-router.use("/auth", createProxyMiddleware({
-    target: "http://localhost:5001",
-    changeOrigin: true,
-}));
-export default router;
-//# sourceMappingURL=routes.js.map
